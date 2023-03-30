4b253d8efb3d6e21f1cd4fd96da131342185ea85
Chi <daniel.le17@hotmail.com>
2023-03-30 18:08:09 -0400

    Fixed database integration, tested write, working. Chi

--- BurgerBurner/ViewController.swift
+++ BurgerBurner/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Foundation
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello Bitches");
         // Do any additional setup after loading the view.
+                
+        // Add a new document in collection "cities" *********Write test for DB******* Working
+        //db.collection("cities").document("LA").setData([
+         //   "name": "Los Angeles",
+           // "state": "CA",
+            //"country": "USA"
+       // ]) { err in
+         //   if let err = err {
+           //     print("Error writing document: \(err)")
+            //} else {
+             //   print("Document successfully written!")
+            //}
+        //}
     }
 
 
